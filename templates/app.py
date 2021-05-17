ac0729f07a1c2dce203bd12353a68c9169f8d065
OlenaBL <35238049+OlenaBL@users.noreply.github.com>
2021-05-17 09:16:29 -0600

    Update app.py

--- templates/app.py
+++ templates/app.py
@@ -10,38 +10,12 @@ with open(f'model/bike_model_xgboost.pkl', 'rb') as f:
 
 app = flask.Flask(__name__, template_folder='templates')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 
 def main():
 
-    if flask.request.method == 'GET':
+    return(flask.render_template('main.html'))
 
-        return(flask.render_template('main.html'))
+if __name__ == '__main__':
 
-    if flask.request.method == 'POST':
-
-        temperature = flask.request.form['temperature']
-
-        humidity = flask.request.form['humidity']
-
-        windspeed = flask.request.form['windspeed']
-
-        input_variables = pd.DataFrame([[temperature, humidity, windspeed]],
-
-                                       columns=['temperature', 'humidity', 'windspeed'],
-
-                                       dtype=float)
-
-        prediction = model.predict(input_variables)[0]
-
-        return flask.render_template('main.html',
-
-                                     original_input={'Temperature':temperature,
-
-                                                     'Humidity':humidity,
-
-                                                     'Windspeed':windspeed},
-
-                                     result=prediction,
-)
-
+    app.run()
