3946ca95bbe7c5d09399941963817f5323fc1cff
OlenaBL <35238049+OlenaBL@users.noreply.github.com>
2021-05-22 21:15:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def main():
         # Make DataFrame for model
         input_variables = pd.DataFrame([[text]], #humidity, windspeed]],
                                        columns=['text'], #'temperature', 'humidity', 'windspeed'],
-                                       dtype=np.float,
+                                       #dtype=np.float,
                                        index=['input'])
 
         # Get the model's prediction
