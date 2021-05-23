06b9d4927f5f13925f64326a7a01ae837b95ea3b
OlenaBL <35238049+OlenaBL@users.noreply.github.com>
2021-05-23 07:16:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 import pickle
 import pandas as pd
+import numpy as np
 from flask import Flask, render_template, request, redirect, url_for
 from numpy.core.arrayprint import dtype_is_implied
 
